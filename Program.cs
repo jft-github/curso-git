7c9217af2c63fa22e523556cb20f5aa7f0c7baa7
Jose Francisco Teixeira <jfteixeira@yahoo.com.br>
2020-03-23 22:33:35 -0300

    Acrescentada a mensagem desejando um bom dia

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace projeto_teste_Git_Aulas99a112
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá mundo!"); 
-            Console.WriteLine("Bom dia!");
-            Console.WriteLine("Como você está hoje?");        
+            Console.WriteLine("Olá mundo!");          
+            Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");       
 
             Console.ReadLine();           
         }
